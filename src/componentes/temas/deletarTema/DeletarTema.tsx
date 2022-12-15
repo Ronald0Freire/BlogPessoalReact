1cbf8f3c2caa5e76e26ba0c67de378e375de26d1
Ronaldo0Freire <ronaldocontami@gmail.com>
2022-12-15 10:51:27 -0300

    alertas

---
+++ src/componentes/temas/deletarTema/DeletarTema.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import { Card, CardActions, CardContent, Button, Typography} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './DeletarTema.css';
+import {useNavigate, useParams } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { buscaId, deleteId } from '../../../paginas/service/Service';
+import Tema from '../../../paginas/model/Tema';
+import { toast } from 'react-toastify';
+
+
+function DeletarTema() {
+    let navigate = useNavigate();
+    const { id } = useParams<{id: string}>();
+    const [token, setToken] = useLocalStorage('token');
+    const [tema, setTema] = useState<Tema>()
+
+    useEffect(() => {
+        if (token == "") {
+          toast('🦄 Você precisa estar logado', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            navigate("/login")
+    
+        }
+    }, [token])
+
+    useEffect(() =>{
+        if(id !== undefined){
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/tema/${id}`, setTema, {
+            headers: {
+              'Authorization': token
+            }
+          })
+        }
+
+        function sim() {
+          navigate('/temas')
+            deleteId(`/tema/${id}`, {
+              headers: {
+                'Authorization': token
+              }
+            });
+            toast('🦄 Tema deletado com sucesso!', {
+              position: "top-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+              });
+          }
+        
+          function nao() {
+            navigate('/temas')
+          }
+          
+  return (
+    <>
+      <Box m={2}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box justifyContent="center">
+              <Typography color="textSecondary" gutterBottom>
+                Deseja deletar o Tema:
+              </Typography>
+              <Typography color="textSecondary">
+                {tema?.descricao}
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
+              <Box mx={2}>
+                <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
+                  Sim
+                </Button>
+              </Box>
+              <Box mx={2}>
+                <Button  onClick={nao} variant="contained" size='large' color="secondary">
+                  Não
+                </Button>
+              </Box>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+}
+export default DeletarTema;
